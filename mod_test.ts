6e4854401e63cde2d53cd2a3a652132ed46740d5
Shahid N. Shah <shahid@shah.org>
2020-07-16 13:00:11 -0400

    Upgrade artifacts-persistence library

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@v0.61.0/testing/asserts.ts";
 import {
   commandComponents,
   CommandLine,
